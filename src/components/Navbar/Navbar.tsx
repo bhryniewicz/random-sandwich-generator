5ceff5a17c7bb0566eb225d81391f2ea95c7bf2a
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2025-01-22 02:51:26 +0100

    feat(MVP-31): add responsive navbar, create another file for routes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,48 +1,62 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import Sandwich from "@/assets/sandwich.png";
-
-const routes = [
-  {
-    href: "/",
-    label: "Generate",
-  },
-  {
-    href: "sandwiches",
-    label: "Sandwiches",
-  },
-  {
-    href: "/",
-    label: "My list",
-  },
-  {
-    href: "/",
-    label: "The best of",
-  },
-  {
-    href: "/",
-    label: "Compose",
-  },
-];
+import { SandwichIcon, XIcon } from "lucide-react";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { routes } from "./routes";
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const mappedRoutes = routes.map((route) => {
+    return <LinkItem href={route.href} label={route.label} key={route.label} />;
+  });
+
   return (
-    <div className="flex justify-between items-center uppercase text-[24px] text-[#471a08] bg-[#f4dac9] px-16 font-luckiest h-[10vh]">
+    <div className="flex justify-between items-center uppercase text-[24px] text-[#471a08] bg-[#f4dac9] px-8 py-4 md:py-0 md:px-16 font-luckiest h-[10vh]">
       <div className="flex items-center">
-        <Image src={Sandwich} width={100} height={100} alt="sandwich logo" />
-        <h1 className="text-xl">
+        <Image
+          src={Sandwich}
+          width={100}
+          height={100}
+          alt="sandwich logo"
+          className="hidden md:block"
+        />
+        <h1 className="text-base leading-4 md:text-xl md:leading-7">
           Random Sandwich
           <br />
           Generator
         </h1>
       </div>
-      <div className="flex items-center gap-8">
-        {routes.map((route) => {
-          return (
-            <LinkItem href={route.href} label={route.label} key={route.label} />
-          );
-        })}
-      </div>
+      <SandwichIcon
+        className="block md:hidden"
+        size={32}
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+      />
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            id="navbar-state"
+            initial={{ opacity: 0.75, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col items-center justify-center gap-8 bg-orange_primary text-white absolute left-0 top-0 h-screen w-full tracking-widest"
+          >
+            <XIcon
+              size={32}
+              strokeWidth={4}
+              className="absolute top-8 right-8"
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+            />
+            {mappedRoutes}
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <div className="hidden md:flex gap-8 tracking-wider">{mappedRoutes}</div>
     </div>
   );
 };
@@ -51,7 +65,7 @@ const LinkItem = ({ href, label }: { href: string; label: string }) => {
   return (
     <Link
       href={`/${href}`}
-      className="mb-auto hover:text-orange_primary hover:underline transition-colors duration-300"
+      className="hover:text-orange_primary hover:underline transition-colors duration-300"
     >
       {label}
     </Link>
---
+++ src/components/Navbar/routes.ts
@@ -0,0 +1,22 @@
+export const routes = [
+  {
+    href: "/",
+    label: "Generate",
+  },
+  {
+    href: "sandwiches",
+    label: "Sandwiches",
+  },
+  {
+    href: "/",
+    label: "My list",
+  },
+  {
+    href: "/",
+    label: "The best of",
+  },
+  {
+    href: "/",
+    label: "Compose",
+  },
+];
